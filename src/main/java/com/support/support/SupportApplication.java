32ab63f4e0633ea68b38e62f6b531c33d898ae4f
soKaradag <so.karadag@gmail.com>
2025-03-13 11:03:38 +0300

    Test Commit

--- src/main/java/com/support/support/SupportApplication.java
+++ src/main/java/com/support/support/SupportApplication.java
@@ -8,6 +8,7 @@ public class SupportApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SupportApplication.class, args);
+		System.out.println("Created Project");
 	}
 
 }
